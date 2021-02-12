c939b0fc8813f3a52f62009444e75f97a8bd63b3
bhooms33 <43778068+bhooms33@users.noreply.github.com>
2021-02-12 19:42:43 +0530

    Update sketch.js

--- CarRacingGame1.0-master/CarRacingGame1.0-master/sketch.js
+++ CarRacingGame1.0-master/CarRacingGame1.0-master/sketch.js
@@ -2,7 +2,7 @@ var canvas, backgroundImage;
 
 var gameState = 0;
 var playerCount;
-var allPlayers;
+
 var distance = 0;
 var database;
 
@@ -19,11 +19,5 @@ function setup(){
 
 
 function draw(){
-  if(playerCount === 4){
-    game.update(1);
-  }
-  if(gameState === 1){
-    clear();
-    game.play();
-  }
+ 
 }
